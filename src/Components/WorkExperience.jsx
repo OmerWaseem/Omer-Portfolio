3d2b047483b0a0ab9bffbd7cb48d817e001b073b
Omer Bin Waseem <omer-bin.waseem@students.mq.edu.au>
2019-10-11 20:43:38 +1100

    Resolution Iphone fix

--- src/Components/WorkExperience.jsx
+++ src/Components/WorkExperience.jsx
@@ -3,26 +3,16 @@ import React from 'react';
 export const WorkExperience = () => (
   <section className="vh-80">
     <div><a name="experience" datatype="section heading"><h1>Work Experience</h1></a></div>
-    <div className="experience-buttons">
-      <p>
-        <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-          Intership at SALAMA (Insurance)
-        </a>
-      </p>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body" style={{background:"rgb(41, 40, 40)"}}>
-          <ul className="list-text">
+        <div className="card card-body experience-card ">
+          <h4 style={{textAlign:"center"}}>Intership at SALAMA (Insurance)</h4>
+          <div className="list-text"><ul >
           <li>Dealing with any inquiries from TPA(Third Party Administrator) about client's medical claim</li>
           <li>Filing Claim Documents</li> 
           <li>Handling Claim Cheques</li> 
           <li>Ensuring Claim Documents are sent to appropriate Processing team</li> 
           <li>Tracking and Updating Claims</li> 
           <li>Notifying TPAs of their client's claim status</li> 
-          </ul>
-          
-        </div>
-      </div>
-    </div>
-    
+          </ul></div>
+          </div> 
   </section>
 )
