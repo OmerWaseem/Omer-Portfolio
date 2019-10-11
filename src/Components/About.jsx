3cc104866c54ef2972113bd56232c392f4ba3f35
Omer Bin Waseem <omer-bin.waseem@students.mq.edu.au>
2019-10-11 18:06:09 +1100

    Fixed mobile resolution

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,19 +3,21 @@ import TextLoop from "react-text-loop"
 
 
 export const About = () => (
+
   <section className="about-me-section col justify-content-center align-items-center w-100 text-align-center align-middle align-content-center">
     <div className="main-heading">About me</div>
-    <h5 style={{ textAlign: "center"}}>
-    <TextLoop springConfig={{ stiffness: 180, damping: 8 }} interval={2000}>
-      <div datatype="secondary heading"style={{ textAlign: "center"}}>Student</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>Cyber Security</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>HTML/CSS</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>Java</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>JavaScript</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>React</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>Linux</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>Unity 2D/3D</div>
-      <div datatype="tertiary heading" style={{ textAlign: "center"}}>Video Games</div>
+    <h5 className="flexbox">
+    <TextLoop springConfig={{ stiffness: 180, damping: 8 }} interval={1500}>
+
+      <div datatype="secondary heading" className="centered-text" >Student</div>
+      <div datatype="tertiary heading" className="centered-text" >Cyber Security</div>
+      <div datatype="tertiary heading" className="centered-text" >HTML/CSS</div>
+      <div datatype="tertiary heading"  className="centered-text">Java</div>
+      <div datatype="tertiary heading" className="centered-text" >JavaScript</div>
+      <div datatype="tertiary heading" className="centered-text" >React</div>
+      <div datatype="tertiary heading" className="centered-text" >Linux</div>
+      <div datatype="tertiary heading" className="centered-text" >Unity 2D/3D</div>
+      <div datatype="tertiary heading" className="centered-text" >Video Games</div>
 
     </TextLoop>
     </h5>
